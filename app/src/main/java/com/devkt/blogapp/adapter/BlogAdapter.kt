b654c48630a68ea047b099ab4110521098fb9bdf
KirtanPatel8 <kirtansantoki12@gmail.com>
2024-10-09 22:03:27 +0530

    Initial Commit

---
+++ app/src/main/java/com/devkt/blogapp/adapter/BlogAdapter.kt
@@ -0,0 +1,39 @@
+package com.devkt.blogapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.devkt.blogapp.Model.BlogItemModel
+import com.devkt.blogapp.databinding.BlogItemsBinding
+
+class BlogAdapter(private val items: List<BlogItemModel>) :
+    RecyclerView.Adapter<BlogAdapter.BlogViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BlogViewHolder {
+        var inflater = LayoutInflater.from(parent.context)
+        val binding = BlogItemsBinding.inflate(inflater, parent, false)
+        return BlogViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: BlogViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class BlogViewHolder(private val binding: BlogItemsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(blogItemModel: BlogItemModel) {
+            binding.heading.text = blogItemModel.heading
+            Glide.with(binding.profile.context).load(blogItemModel.imageUrl).into(binding.profile)
+            binding.userName.text = blogItemModel.userName
+            binding.date.text = blogItemModel.date
+            binding.post.text = blogItemModel.post
+            binding.likeCount.text = blogItemModel.likeCount.toString()
+        }
+
+    }
+}
